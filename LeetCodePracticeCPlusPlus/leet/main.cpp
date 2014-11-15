4a66e50d407190671494ac110326bcce4e448856
sidawang <sida.wang.cs@gmail.com>
2014-11-14 16:23:09 -0800

    first commit mac

---
+++ LeetCodePracticeCPlusPlus/leet/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include "test.h"
+using namespace std;
+// Driver program to test abve functions
+int main() {
+	string s = "";
+	s += 2 + '0';
+	//cout<< std::atoi(s.c_str());
+	cout << s;
+	print();
+	/*   int mat[2][3] = { {1, 2, 3}, {4, 5, 6} };
+	 printAllPaths(*mat, 2, 3);
+	 return 0; */
+}
--- LeetCodePracticeCPlusPlus/leet/test.cpp
+++ LeetCodePracticeCPlusPlus/leet/test.cpp
@@ -61,15 +61,6 @@ void printAllPaths(int *mat, int m, int n)
     int *path = new int[m+n];
     printAllPathsUtil(mat, 0, 0, m, n, path, 0);
 }
-
-// Driver program to test abve functions
-int main()
-{
-	string s ="";
-	s+= 1+'0';
-	//cout<< std::atoi(s.c_str());
-	cout<<s;
- /*   int mat[2][3] = { {1, 2, 3}, {4, 5, 6} };
-    printAllPaths(*mat, 2, 3);
-    return 0; */
+void print(){
+	cout << "test";
 }
